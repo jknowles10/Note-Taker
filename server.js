b2f27b2de597f9f5fbd8256638930974fcfd6389
julienneknowles <julienneknowles@gmail.com>
2024-07-30 22:40:39 -0500

    final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const db = require('./db/db.json');
+const db = require('./Develop/db/db.json');
 
 const PORT = 3001; 
 
